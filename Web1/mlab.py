b8447fdf1a17bc15acdb314721fa3be8948c2bd6
DESKTOP-Q0ELJP1\huong-pc <imissyoutlhp@gmail.com>
2017-06-22 15:26:22 +0700

    E nộp bài

---
+++ Web1/mlab.py
@@ -0,0 +1,22 @@
+import mongoengine
+
+#mongodb://<dbuser>:<dbpassword>@ds129402.mlab.com:29402/thuoclao
+
+host = "ds129402.mlab.com"
+port = 29402
+db_name = "thuoclao"
+user_name = "admin"
+password = "admin"
+
+
+def connect():
+    mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
+
+def list2json(l):
+    import json
+    return [json.loads(item.to_json()) for item in l]
+
+
+def item2json(item):
+    import json
+    return json.loads(item.to_json())
